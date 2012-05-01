f7d9055cc7445ddd6a03f063dbb93229b5ae56b3
Merwan Achibet <merwandonut@hotmail.com>
2012-05-01 17:08:28 +0200

    Add easter egg

--- utils/konami.js
+++ utils/konami.js
@@ -8,7 +8,6 @@ var keys = {
 };
 
 var code = ['up', 'up', 'down', 'down', 'left', 'right', 'left', 'right', 'b', 'a'];
-
 var cursor = 0;
 
 $(window).keyup(function(event) {
@@ -30,5 +29,50 @@ $(window).keyup(function(event) {
 
 function easterEgg() {
 
-	// Do stuff!
+	// PWEE PWUU PWEE PWUU PWEE PWUU
+
+	$audio = $('<audio></audio>')
+		.attr({autoplay: 'autoplay'})
+		.appendTo('body');
+
+	$sound = $('<source></source>')
+		.attr({src: 'images/alarm.ogg', type: 'audio/ogg'})
+		.appendTo($audio);
+
+	// DU DUDU DUDU DUUUU
+
+	setTimeout(function() {
+
+		$gentleman = $('<img/>')
+			.attr('src', 'images/gentleman.png')
+			.hide()
+			.appendTo('body')
+			.load(
+				function() {
+					blink($(this), 8, 230);
+				}
+			);
+
+		$gentleman.css({
+			position: 'fixed',
+			bottom: 0,
+			right: 0
+		});
+
+	}, 10500);
+}
+
+function blink($element, count, duration) {
+
+	if(count < 0)
+		return;
+
+	var nextCall = function() {
+		blink($element, count - 1, duration);
+	};
+
+	if($element.is(':visible'))
+		$element.fadeOut(duration, nextCall);
+	else
+		$element.fadeIn(duration, nextCall);
 }
